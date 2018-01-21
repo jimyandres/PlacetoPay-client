c2aa864637bacdc927c9f3cbdb932d5d3c50f1b4
jimyandres <jimyandres@utp.edu.co>
2018-01-21 12:59:45 -0500

    Refactoring routes

--- routes/web.php
+++ routes/web.php
@@ -13,32 +13,88 @@
 
 use App\Http\Controllers\PaymentController;
 
-// Presenta la página de bienvenida
+/**
+ * Presenta la página de bienvenida
+ */
 Route::get('/', 'PaymentController@index');
 
-// (paso 1.Listar Bancos)
-// Presenta la página de inicio
-Route::get('/payment', 'PaymentController@startView');
+/**
+ * Grupo de rutas englobadas con Payment
+ */
+Route::group(['as' => 'payment::', 'prefix' => 'payment'], function () {
 
-// Se obtiene al banco (y tipo de cuenta) seleccionada en la página de
-// inicio, redirecciona a la página de verificación si el banco seleccionado existe
-Route::post('/payment', 'PaymentController@startRequest');
+    /**
+     * (paso 1.Listar Bancos)
+     */
+    Route::group([],function () {
+        /**
+         * Presenta la página de inicio
+         */
+        Route::get('', [
+            'as' => 'start',
+            'uses' => 'PaymentController@startView'
+        ]);
 
-// (paso 2.Página de verificación)
-// Se obtiene la página de verificación
-Route::get('/payment/verification', 'PaymentController@verificationView');
+        /**
+         * Se obtiene al banco (y tipo de cuenta) seleccionada en la página de
+         * inicio, redirecciona a la página de verificación
+         */
+        Route::post('', [
+            'as' => 'start',
+            'uses' => 'PaymentController@startRequest'
+        ]);
+    });
 
-// Se obtienen los datos según la opción seleccionada (Registrarse o continuar) y redirecciona
-Route::post('/payment/verification', 'PaymentController@verificationRequest');
+    /**
+     * (paso 2.Página de verificación)
+     */
+    Route::group([],function () {
+        /**
+         * Se obtiene la página de verificación
+         */
+        Route::get('verification', [
+            'as' => 'verification',
+            'uses' => 'PaymentController@verificationView'
+        ]);
+        /**
+         * Se obtienen los datos según la opción seleccionada (Registrarse o continuar)
+         * y redirecciona a la página correspondiente
+         */
+        Route::post('verification', [
+            'as' => 'verification',
+            'uses' => 'PaymentController@verificationRequest'
+        ]);
+    });
 
-// (paso 2.1.Registro de correo electrónico)
-// Presenta la página de registro
-Route::get('/payment/register', 'PaymentController@registerView');
+    /**
+     * (paso 2.1.Registro de correo electrónico)
+     */
+    Route::group([],function () {
+        /**
+         * Presenta la página de registro
+         */
+        Route::get('register', [
+            'as' => 'register_account',
+            'uses' => 'PaymentController@registerView'
+        ]);
 
-// Se obtienen los datos para el registro y redirecciona
-Route::post('/payment/register', 'PaymentController@registerRequest');
+        /**
+         * Se obtienen los datos para el registro y redirecciona
+         */
+        Route::post('register', [
+            'as' => 'register_account',
+            'uses' => 'PaymentController@registerRequest'
+        ]);
+    });
 
-// (paso 3.Página simuladora de Banco)
+    /**
+     * (paso 4.Presentación de resultado de operación
+     */
+    Route::get('result', [
+        'as' => 'result_payment',
+        'uses' => 'PaymentController@result'
+    ]);
+});
 
-// (paso 4.Presentación de resultado de operación
-Route::get('/payment/result', 'PaymentController@result');
+// Rutas temporales
+Route::get('/{type}', 'PaymentController@webServiceInfo');
